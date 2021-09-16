7233d44e29ae4b4c7586f8c8e8209d5b2cbbb449
egor.kosov <egor.kosov@brandmaker.com>
2021-09-16 10:47:40 +0700

    added Listener for Allure

--- src/test/java/ru/config/TestBase.java
+++ src/test/java/ru/config/TestBase.java
@@ -1,7 +1,10 @@
 package ru.config;
 
+import io.qameta.allure.selenide.AllureSelenide;
+
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -12,6 +15,7 @@ public class TestBase
     static void setUp()
     {
         Configuration.startMaximized = true;
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
     @AfterAll
